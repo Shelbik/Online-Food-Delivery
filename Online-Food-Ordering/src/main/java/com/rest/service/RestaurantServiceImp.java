104bfbeecc5e722f4deec458067b232bdec86d85
Vitalii Martiniuc <vitalimartiniuc@gmail.com>
2024-08-16 23:21:59 +0200

    v1

--- Online-Food-Ordering/src/main/java/com/rest/service/RestaurantServiceImp.java
+++ Online-Food-Ordering/src/main/java/com/rest/service/RestaurantServiceImp.java
@@ -122,9 +122,21 @@ public class RestaurantServiceImp implements RestaurantService {
         restaurantDto.setTitle(restaurant.getName());
         restaurantDto.setId(restaurantId);
 
-        if (user.getFavorites().contains(restaurantDto)) {
-            user.getFavorites().remove(restaurantDto);
-        } else user.getFavorites().add(restaurantDto);
+        boolean isFavorited = false;
+        List<RestaurantDto> favorites = user.getFavorites();
+        for (RestaurantDto favorite : favorites) {
+            if (favorite.getId().equals(restaurantId)) {
+                isFavorited = true;
+                break;
+            }
+        }
+
+        if (isFavorited) {
+            favorites.removeIf(favorite -> favorite.getId().equals(restaurantId));
+        } else {
+            favorites.add(restaurantDto);
+        }
+
         usersRepository.save(user);
         return restaurantDto;
     }
