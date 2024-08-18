2e51196373db894201ac1007120158d0d9b0af1e
Vitalii Martiniuc <vitalimartiniuc@gmail.com>
2024-08-18 16:58:51 +0200

    v1

--- Online-Food-Ordering/src/main/java/com/rest/model/IngredientCategory.java
+++ Online-Food-Ordering/src/main/java/com/rest/model/IngredientCategory.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class IngredientsCategory {
+public class IngredientCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
--- Online-Food-Ordering/src/main/java/com/rest/model/IngredientsItem.java
+++ Online-Food-Ordering/src/main/java/com/rest/model/IngredientsItem.java
@@ -19,7 +19,7 @@ public class IngredientsItem {
     private String name;
 
     @ManyToOne
-    private IngredientsCategory category;
+    private IngredientCategory category;
 
     @JsonIgnore
     @ManyToOne
